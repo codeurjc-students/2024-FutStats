8dc270de21adce5b1ff2d9ccbb0ebf4123e95b25
DanielPandolfoMendoza <df.pandolfo.2020@alumnos.urjc.es>
2024-09-22 20:31:21 +0200

    ProjectCreation

---
+++ futstats/src/main/java/com/tfg/futstats/FutstatsApplication.java
@@ -0,0 +1,13 @@
+package com.tfg.futstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutstatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutstatsApplication.class, args);
+	}
+
+}
---
+++ futstats/src/test/java/com/tfg/futstats/FutstatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfg.futstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutstatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
