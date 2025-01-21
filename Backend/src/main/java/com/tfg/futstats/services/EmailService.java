1f302fb9648736d446593bedcbf775942e90d827
Daniel Francisco Pandolfo Mendoza <146677531+DanielFPandolfoMendoza@users.noreply.github.com>
2025-01-21 17:43:55 +0100

    changes in email service

--- Backend/src/main/java/com/tfg/futstats/services/EmailService.java
+++ Backend/src/main/java/com/tfg/futstats/services/EmailService.java
@@ -3,6 +3,10 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 
+import org.springframework.stereotype.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 @Service
 public class EmailService {
 
