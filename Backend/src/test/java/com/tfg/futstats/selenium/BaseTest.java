58f901e3eb28904c79617f006c36876f57817485
Daniel Francisco Pandolfo Mendoza <146677531+DanielFPandolfoMendoza@users.noreply.github.com>
2025-02-25 09:54:12 +0100

    Update BaseTest.java

--- Backend/src/test/java/com/tfg/futstats/selenium/BaseTest.java
+++ Backend/src/test/java/com/tfg/futstats/selenium/BaseTest.java
@@ -1,26 +1,34 @@
 package com.tfg.futstats.selenium;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 
 public abstract class BaseTest {
 
     protected WebDriver driver;
 
     @BeforeEach
-public void setUp() throws Exception {
-    driver = new ChromeDriver();
-    driver.manage().window().maximize();
-}
+    public void setUp() throws Exception {
+        ChromeOptions options = new ChromeOptions();
+        
+        Path tempProfile = Files.createTempDirectory("chrome-profile");
+        options.addArguments("--user-data-dir=" + tempProfile.toAbsolutePath().toString());
+
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
+    }
 
     @AfterEach
     public void tearDown() {
         if (driver != null) {
             driver.quit();
         }
-        
     }
 }
