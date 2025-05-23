e2a63287accf1510c9b188f2ec991470151bed35
Daniel Francisco Pandolfo Mendoza <146677531+DanielFPandolfoMendoza@users.noreply.github.com>
2025-05-23 12:41:59 +0200

    Update PlayerMatchFormTest.java

--- Backend/src/test/java/com/tfg/futstats/selenium/admin/playerMatch/PlayerMatchFormTest.java
+++ Backend/src/test/java/com/tfg/futstats/selenium/admin/playerMatch/PlayerMatchFormTest.java
@@ -18,6 +18,8 @@ public class PlayerMatchFormTest extends BaseTest {
     public void testLoginFunctionality() {
         driver.get("https://localhost:" + this.port + "/playerMatch/1");
 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+
         WebElement usernameField = driver.findElement(By.name("username"));
         WebElement passwordField = driver.findElement(By.name("password"));
         WebElement loginButton = driver.findElement(By.xpath("//button[contains(text(), 'Iniciar Sesión')]"));
